ef80bf85e1ffdc532d1e815c92a3c1491a64acde
ac2917 <aditya.chaturvedi17@imperial.ac.uk>
2019-01-20 12:04:56 +0000

    adding question description

--- twosum.py
+++ twosum.py
@@ -1,3 +1,11 @@
+"""
+STATEMENT
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+EXAMPLES
+[2, 7, 11, 15], 9 -> [0,1]
+"""
+
 def twosum(array_nums, target):
 	sum_dict = {}
 	for i in range(len(array_nums)):
